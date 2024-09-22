9d3e167d58fb32f8039922258120631e5da1cd5b
RenPen3 <wrenwiththepen@gmail.com>
2024-09-21 23:14:54 -0700

    I updated the footer

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 const mobile = document.querySelector('.mobile');
 const ham = document.querySelector('.ham-menu');
+const upArrow = document.getElementById('up');
 
-
+console.log(upArrow);
 ham.addEventListener('click', () => {
   mobile.classList.toggle('show');
+})
+
+upArrow.addEventListener('mouseenter', () => {
+  upArrow.style.marginBottom = '2em';
 })
